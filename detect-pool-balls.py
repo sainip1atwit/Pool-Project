a61716de8aa8fc5eb2ab0ea17035c313842b9de9
sainip1atwit <sainip1@wit.edu>
2025-01-20 23:04:31 -0500

    switched lower and upper bounds

--- detect-pool-balls.py
+++ detect-pool-balls.py
@@ -1,18 +1,6 @@
 import cv2 as cv
 import numpy as np
 
-def get_limits(color):
-    c = np.uint8([[color]])
-    hsvC = cv.cvtColor(c, cv.COLOR_BGR2HSV)
-
-    lowerLimit = hsvC[0][0][0] - 10, 100, 100
-    upperLimit = hsvC[0][0][0] + 10, 255, 255
-
-    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
-    upperLimit = np.array(upperLimit, dtype=np.uint8)
-
-    return lowerLimit, upperLimit
-
 def mask_obj():
     frame = cv.imread('data/poolpic-1ball.jpg')
 
@@ -22,10 +10,10 @@ def mask_obj():
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     cv.imshow('hsv', hsv)
 
-    yellow = [0, 255, 255]
-    lower, upper = get_limits(yellow)
+    lower = np.array([0,0,168])
+    upper = np.array([172, 111, 255])
 
-    mask = cv.inRange(hsv, upper, lower)
+    mask = cv.inRange(hsv, lower, upper)
     cv.imshow('Mask', mask)
 
     cv.waitKey(0)
